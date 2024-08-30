fc45cb42129f6a179955d7605058bef93c213664
Wellington <wellingtoneugenio@gmail.com>
2024-08-30 16:05:49 -0300

    Service mail concluido

--- src/main/java/com/ms/email/models/EmailModel.java
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -11,13 +11,63 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
 @Entity
 @Table(name = "TB_EMAIL")
+@Getter
+@Setter
 public class EmailModel implements Serializable {
 
+	public Long getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(Long emailId) {
+		this.emailId = emailId;
+	}
+	public String getOwnerRef() {
+		return ownerRef;
+	}
+	public void setOwnerRef(String ownerRef) {
+		this.ownerRef = ownerRef;
+	}
+	public String getEmailFrom() {
+		return emailFrom;
+	}
+	public void setEmailFrom(String emailFrom) {
+		this.emailFrom = emailFrom;
+	}
+	public String getEmailTo() {
+		return emailTo;
+	}
+	public void setEmailTo(String emailTo) {
+		this.emailTo = emailTo;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public LocalDateTime getSendDateEmail() {
+		return sendDateEmail;
+	}
+	public void setSendDateEmail(LocalDateTime sendDateEmail) {
+		this.sendDateEmail = sendDateEmail;
+	}
+	public StatusEmail getStatusEmail() {
+		return statusEmail;
+	}
+	public void setStatusEmail(StatusEmail statusEmail) {
+		this.statusEmail = statusEmail;
+	}
 	private static final long serialVersionUID = 1L;
 	
 	@Id
